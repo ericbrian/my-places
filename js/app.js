f333f8660a7ef6162ce16d17c2097ad813390704
Eric Coffman <ericbrian@gmail.com>
2018-08-19 10:11:44 -0400

    Conver to using Google maps. Add more locations.

--- js/app.js
+++ js/app.js
@@ -3,37 +3,39 @@ var map;
 function initMap() {
   console.log("Init Map.");
   map = new google.maps.Map(document.getElementById("mapid"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8
+    center: { lat: 36.5, lng: 36.0 },
+    zoom: 2
   });
 }
-
+// http://jsfiddle.net/EricBrian/w96a0yrs/
 $(document).ready(function() {
   console.log("document is ready");
-  //   $.ajax({
-  //     type: "GET",
-  //     url: "/js/places.json",
-  //     dataType: "json",
-  //     mimeType: "application/json"
-  //   })
-  //     .done(myplaces => {
-  //       console.log(myplaces);
-  //       for (var i = 0; i < myplaces.length; i++) {
-  //         var x = myplaces[i][0],
-  //           y = myplaces[i][1],
-  //           line1 = myplaces[i][4],
-  //           line2 = myplaces[i][5],
-  //           line =
-  //             line2 != null && line2.length > 0
-  //               ? line1 + "<br />(" + line2 + ")"
-  //               : line1;
-  //         console.log(x, y, line);
-  //         L.marker([x, y])
-  //           .addTo(map)
-  //           .bindPopup(line);
-  //       }
-  //     })
-  //     .fail((jqXHR, textStatus, errorThrown) => {
-  //       console.error(textStatus, errorThrown);
-  //     });
+  $.ajax({
+    type: "GET",
+    url: "/js/places.json",
+    dataType: "json",
+    mimeType: "application/json"
+  })
+    .done(myplaces => {
+      console.log(myplaces);
+      for (var i = 0; i < myplaces.length; i++) {
+        var x = myplaces[i][0],
+          y = myplaces[i][1],
+          line1 = myplaces[i][4],
+          line2 = myplaces[i][5],
+          line =
+            line2 != null && line2.length > 0
+              ? line1 + "<br />(" + line2 + ")"
+              : line1;
+
+        var marker = new google.maps.Marker({
+          position: { lat: x, lng: y },
+          map: map,
+          title: line
+        });
+      }
+    })
+    .fail((jqXHR, textStatus, errorThrown) => {
+      console.error(textStatus, errorThrown);
+    });
 });
