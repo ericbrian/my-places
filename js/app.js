28c8289b0e0ed7ce0ca95e9e572707989aa5167d
Eric Coffman <ericbrian@gmail.com>
2018-08-20 12:29:48 -0400

    Determine center by using bounds.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 var map;
-var locationInfo;
+var markerGroups;
+var bounds;
 
 var icons = {
   Birth:
@@ -18,11 +19,7 @@ initMap = () => {
     center: { lat: 36.5, lng: 36.0 },
     zoom: 2
   });
-  var markerGroups = new google.maps.MVCObject();
-  markerGroups.set("Home", map);
-  markerGroups.set("Pleasure", map);
-  markerGroups.set("Birth", map);
-  markerGroups.set("Work", map);
+  bounds = new google.maps.LatLngBounds();
 };
 
 // http://jsfiddle.net/EricBrian/w96a0yrs/
@@ -36,35 +33,56 @@ $(document).ready(function() {
   })
     .done(myplaces => {
       console.log(myplaces);
-      for (var i = 0; i < myplaces.length; i++) {
-        var x = myplaces[i][1],
-          y = myplaces[i][2],
-          title = myplaces[i][0],
-          activityicon = icons[myplaces[i][3]],
-          category = myplaces[i][3],
-          content = myplaces[i][4];
-
-        var marker = new google.maps.Marker({
-          position: { lat: x, lng: y },
-          icon: activityicon,
-          title: title,
-          map: map
-        });
-
-        var infowindow = new google.maps.InfoWindow();
-        google.maps.event.addListener(
-          marker,
-          "click",
-          (function(marker, content, infowindow) {
-            return function() {
-              infowindow.setContent(content);
-              infowindow.open(map, marker);
-            };
-          })(marker, content, infowindow)
-        );
-      }
+      setMarkerGroups(myplaces);
+      setMarkersOnMap(myplaces);
     })
     .fail((jqXHR, textStatus, errorThrown) => {
       console.error(textStatus, errorThrown);
     });
 });
+
+setMarkerGroups = myplaces => {
+  var uniqueGroup = [];
+  for (var i = 0; i < myplaces.length; i++) {
+    var groupName = myplaces[i][3];
+    if (uniqueGroup.indexOf(groupName) == -1) uniqueGroup.push(groupName);
+  }
+
+  markerGroups = new google.maps.MVCObject();
+  for (var i = 0; i < uniqueGroup.length; i++) {
+    markerGroups.set(uniqueGroup[i], map);
+  }
+};
+
+setMarkersOnMap = myplaces => {
+  for (var i = 0; i < myplaces.length; i++) {
+    var x = myplaces[i][1],
+      y = myplaces[i][2],
+      title = myplaces[i][0],
+      activityicon = icons[myplaces[i][3]],
+      content = myplaces[i][4];
+
+    var marker = new google.maps.Marker({
+      position: { lat: x, lng: y },
+      icon: activityicon,
+      title: title,
+      map: map
+    });
+
+    bounds.extend(marker.position);
+
+    var infowindow = new google.maps.InfoWindow();
+    google.maps.event.addListener(
+      marker,
+      "click",
+      (function(marker, content, infowindow) {
+        return function() {
+          infowindow.setContent(content);
+          infowindow.open(map, marker);
+        };
+      })(marker, content, infowindow)
+    );
+
+    map.fitBounds(bounds);
+  }
+};
